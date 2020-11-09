7352d70675b19486a5084916a5481696089edcb6
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2020-11-09 20:20:29 +0100

    basic setup

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("js");
+
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+  });
+
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    templateFormats: ["njk", "html"],
+  });
+
+  return {
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "html"],
+  };
+};
